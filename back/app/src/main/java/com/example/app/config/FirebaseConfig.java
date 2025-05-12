9ad30c0181bcb0f68ca6272628ed7db3b0f5e95f
ErnestoPuga <A00572845@tec.mx>
2025-05-12 16:25:31 -0600

    Config Firebase

---
+++ back/app/src/main/java/com/example/app/config/FirebaseConfig.java
@@ -0,0 +1,33 @@
+package com.example.app.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initializeFirebase() {
+        try {
+            Dotenv dotenv = Dotenv.load();
+            String path = dotenv.get("FIREBASE_CONFIG_PATH");
+
+            FileInputStream serviceAccount = new FileInputStream(path);
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
